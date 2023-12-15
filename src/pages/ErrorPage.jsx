908b66bf81be70e25f4c57f0b78cfdb9e21c9a95
Lucas Escobedo <escobedolucas123@gmail.com>
2023-12-15 19:53:03 -0300

    pagina 404
    ta corto, no era mucho

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "../css/ErrorPage.css";
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
+    <div className='text-center'>
+      <h1 className='texto-de-error'>404 - No Encontrado</h1>
+      <p>Sorry, la pagina que buscas parece que no existe!.</p>
+      <Link to="/">Go back to the main page</Link>
+    </div>
+  );
 }
 
 export default ErrorPage
