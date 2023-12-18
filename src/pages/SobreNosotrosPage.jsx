fe968235ba5e44e82d658c783d1d273b9f996f4b
Lucas Escobedo <escobedolucas123@gmail.com>
2023-12-18 14:33:16 -0300

    fix a sobre nosotros

--- src/pages/SobreNosotrosPage.jsx
+++ src/pages/SobreNosotrosPage.jsx
@@ -19,8 +19,6 @@ const SobreNosotros = () => {
         <Card.Title>Bruno Casavalle</Card.Title>
         <hr />
         <Card.Text>
-          <p>
-          hola me llamo Bruno Casavalle, soy un apasionado de la computacion y tambien de los deportes, estoy estudiando en la UTN la tecnicatura en programacion y soy un estudiante de rolling</p>
           <a href="#" className='iconosSobreNosotros'><FaGithub size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaGit size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaLinkedin size={28} /></a>
         </Card.Text>
         </Card.Body>
@@ -33,11 +31,9 @@ const SobreNosotros = () => {
          <Card style={{ width: '18rem' }} className='cardSobreNosotros'>
          <Card.Img className='rounded-circle' variant="top" src='src\img\ad830444-8f7b-46f7-a281-147e38a27d83.jpeg'/>
          <Card.Body className='cardDetails text-center'>
-        <Card.Title>Bruno Casavalle</Card.Title>
+        <Card.Title>Lucas Escobedo</Card.Title>
         <hr />
         <Card.Text>
-          <p>
-          hola me llamo Bruno Casavalle, soy un apasionado de la computacion y tambien de los deportes, estoy estudiando en la UTN la tecnicatura en programacion y soy un estudiante de rolling</p>
           <a href="#" className='iconosSobreNosotros'><FaGithub size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaGit size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaLinkedin size={28} /></a>
         </Card.Text>
         </Card.Body>
@@ -50,11 +46,9 @@ const SobreNosotros = () => {
          <Card style={{ width: '18rem' }} className='cardSobreNosotros'>
          <Card.Img className='rounded-circle' variant="top" src='src\img\ad830444-8f7b-46f7-a281-147e38a27d83.jpeg'/>
          <Card.Body className='cardDetails text-center'>
-        <Card.Title>Bruno Casavalle</Card.Title>
+        <Card.Title>Hugo Torres</Card.Title>
         <hr />
         <Card.Text>
-          <p>
-          hola me llamo Bruno Casavalle, soy un apasionado de la computacion y tambien de los deportes, estoy estudiando en la UTN la tecnicatura en programacion y soy un estudiante de rolling</p>
           <a href="#" className='iconosSobreNosotros'><FaGithub size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaGit size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaLinkedin size={28} /></a>
         </Card.Text>
         </Card.Body>
@@ -67,11 +61,9 @@ const SobreNosotros = () => {
          <Card style={{ width: '18rem' }} className='cardSobreNosotros'>
          <Card.Img className='rounded-circle' variant="top" src='src\img\ad830444-8f7b-46f7-a281-147e38a27d83.jpeg'/>
          <Card.Body className='cardDetails text-center'>
-        <Card.Title>Bruno Casavalle</Card.Title>
+        <Card.Title>Franco Chaile</Card.Title>
         <hr />
         <Card.Text>
-          <p>
-          hola me llamo Bruno Casavalle, soy un apasionado de la computacion y tambien de los deportes, estoy estudiando en la UTN la tecnicatura en programacion y soy un estudiante de rolling</p>
           <a href="#" className='iconosSobreNosotros'><FaGithub size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaGit size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaLinkedin size={28} /></a>
         </Card.Text>
         </Card.Body>
@@ -84,45 +76,9 @@ const SobreNosotros = () => {
          <Card style={{ width: '18rem' }} className='cardSobreNosotros'>
          <Card.Img className='rounded-circle' variant="top" src='src\img\ad830444-8f7b-46f7-a281-147e38a27d83.jpeg'/>
          <Card.Body className='cardDetails text-center'>
-        <Card.Title>Bruno Casavalle</Card.Title>
+        <Card.Title>Rodolfo Cabrera</Card.Title>
         <hr />
         <Card.Text>
-          <p>
-          hola me llamo Bruno Casavalle, soy un apasionado de la computacion y tambien de los deportes, estoy estudiando en la UTN la tecnicatura en programacion y soy un estudiante de rolling</p>
-          <a href="#" className='iconosSobreNosotros'><FaGithub size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaGit size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaLinkedin size={28} /></a>
-        </Card.Text>
-        </Card.Body>
-        </Card>
-
-        </div>
-
-        <div className='bodyCards col-xs-12 col-md-6 col-lg-3'>
-
-         <Card style={{ width: '18rem' }} className='cardSobreNosotros'>
-         <Card.Img className='rounded-circle' variant="top" src='src\img\ad830444-8f7b-46f7-a281-147e38a27d83.jpeg'/>
-         <Card.Body className='cardDetails text-center'>
-        <Card.Title>Bruno Casavalle</Card.Title>
-        <hr />
-        <Card.Text>
-          <p>
-          hola me llamo Bruno Casavalle, soy un apasionado de la computacion y tambien de los deportes, estoy estudiando en la UTN la tecnicatura en programacion y soy un estudiante de rolling</p>
-          <a href="#" className='iconosSobreNosotros'><FaGithub size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaGit size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaLinkedin size={28} /></a>
-        </Card.Text>
-        </Card.Body>
-        </Card>
-
-        </div>
-
-        <div className='bodyCards col-xs-12 col-md-6 col-lg-3'>
-
-         <Card style={{ width: '18rem' }} className='cardSobreNosotros'>
-         <Card.Img className='rounded-circle' variant="top" src='src\img\ad830444-8f7b-46f7-a281-147e38a27d83.jpeg'/>
-         <Card.Body className='cardDetails text-center'>
-        <Card.Title>Bruno Casavalle</Card.Title>
-        <hr />
-        <Card.Text>
-          <p>
-          hola me llamo Bruno Casavalle, soy un apasionado de la computacion y tambien de los deportes, estoy estudiando en la UTN la tecnicatura en programacion y soy un estudiante de rolling</p>
           <a href="#" className='iconosSobreNosotros'><FaGithub size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaGit size={28} /></a> <a href="#" className='iconosSobreNosotros'><FaLinkedin size={28} /></a>
         </Card.Text>
         </Card.Body>
