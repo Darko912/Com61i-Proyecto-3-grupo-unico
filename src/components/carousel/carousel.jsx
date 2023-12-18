3927dcddf4b80d9665c3e62d5b2442098e297204
Lucas Escobedo <escobedolucas123@gmail.com>
2023-12-18 17:14:33 -0300

    arreglo las imagenes

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -8,14 +8,14 @@ const CarouselComponent = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="../../img/ImgMainPage/CarruselImagenes.jpg"
+          src="https://plus.unsplash.com/premium_photo-1670984940113-f3aa1cd1309a?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmVzdGF1cmFudGVzfGVufDB8fDB8fHww"
           alt="First slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="../../img/ImgMainPage/CarruselImg2.jpg"
+          src="https://resizer.otstatic.com/v2/photos/wide-huge/1/31852776.jpg"
           alt="Second slide"
         />
       </Carousel.Item>
